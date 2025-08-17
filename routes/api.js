f34a68724a4148de5c18296e79c6c0068ab0ad45
Salmaan Saeed <salsaeed999@gmail.com>
2025-08-17 19:17:59 -0400

    implemented user login for the /api/login route

--- routes/api.js
+++ routes/api.js
@@ -35,8 +35,34 @@ router.post("/register", async function (req, res, next) {
   }
 });
 
-router.get("/login", function (req, res, next) {
-  res.send("respond with a resource");
+router.post("/login", async function (req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  const data = req.body;
+  const username = data.username;
+  const password = data.password;
+
+  const find_user_via_username = await User.findOne({ where: { username: username } });
+  const find_user_via_email = await User.findOne({ where: { email: username } });
+
+  if (find_user_via_username !== null) {
+    const match = await bcrypt.compare(password, find_user_via_username.password)
+    if (match == false) {
+      res.send("Error Login!")
+    } else if (match == true) {
+      json_response = {"msg": "Login Success", "id": find_user_via_username.id, "code": find_user_via_username.code}
+      res.json(json_response)
+    }
+  } else if (find_user_via_email !== null) {
+    const match = await bcrypt.compare(password, find_user_via_email.password)
+    if (match == false) {
+      res.send("Error Login!")
+    } else if (match == true) {
+      json_response = {"msg": "Login Success", "id": find_user_via_email.id, "code": find_user_via_email.code}
+      res.json(json_response)
+    }
+  } else {
+    res.send("Error Login")
+  }
 });
 
 module.exports = router;
