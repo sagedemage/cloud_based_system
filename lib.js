c95e9f9b95aae1dffadfa67228318316cf919423
Salmaan Saeed <salsaeed999@gmail.com>
2025-08-20 17:58:39 -0400

    linted and formatted the codebase

--- lib.js
+++ lib.js
@@ -2,23 +2,23 @@ const { randomBytes } = require("node:crypto");
 
 function random_string(length) {
   if (length % 2 !== 0) {
-    length += 1
+    length += 1;
   }
-  return randomBytes(length / 2).toString("hex")
+  return randomBytes(length / 2).toString("hex");
 }
 
 function get_cookie_value(cookie, cname) {
-  let values = {}
-  let temp_values = cookie.split("; ")
+  let values = {};
+  let temp_values = cookie.split("; ");
   for (const value of temp_values) {
-    let key_value = value.split("=")
+    let key_value = value.split("=");
     const key = key_value[0];
-    values[key] = key_value[1]
+    values[key] = key_value[1];
   }
   return values[cname];
 }
 
 module.exports = {
-    random_string,
-    get_cookie_value
-}
+  random_string,
+  get_cookie_value,
+};
